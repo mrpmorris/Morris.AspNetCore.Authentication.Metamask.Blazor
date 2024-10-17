9f853d05e8cdbcd1f1b84b70b9eeb8ebf718afcd
Peter Morris <mrpmorris@gmail.com>
2024-10-17 16:56:37 +0100

    , forceLoad: true

--- Source/Lib/Morris.AspNetCore.Authentication.Metamask.Blazor/Components/Pages/Metamask/SelectWallet.razor.cs
+++ Source/Lib/Morris.AspNetCore.Authentication.Metamask.Blazor/Components/Pages/Metamask/SelectWallet.razor.cs
@@ -6,34 +6,37 @@ namespace Morris.AspNetCore.Authentication.Metamask.Blazor.Components.Pages.Meta
 
 public partial class SelectWallet
 {
-    [SupplyParameterFromQuery(Name = "Accounts")]
-    public string? QuerystringAccounts { get; set; }
+	[SupplyParameterFromQuery(Name = "Accounts")]
+	public string? QuerystringAccounts { get; set; }
 
-    [SupplyParameterFromForm]
-    public required ViewModel Model { get; set; } = null!;
+	[SupplyParameterFromForm]
+	public required ViewModel Model { get; set; } = null!;
 
-    [SupplyParameterFromQuery(Name = MetamaskHandler.StateKey)]
-    public required string State { get; set; } = null!;
+	[SupplyParameterFromQuery(Name = MetamaskHandler.StateKey)]
+	public required string State { get; set; } = null!;
 
-    private string[] Accounts = [];
-    private EditForm EditForm = null!;
+	private string[] Accounts = [];
+	private EditForm EditForm = null!;
 
-    protected override void OnParametersSet()
-    {
-        base.OnParametersSet();
-        if (!string.IsNullOrWhiteSpace(QuerystringAccounts))
-            Accounts = QuerystringAccounts.Split(',');
-        Model ??= new();
-    }
-    private void Save()
-    {
-        bool isValid = EditForm.EditContext!.Validate();
-        if (!isValid)
-            return;
+	protected override void OnParametersSet()
+	{
+		base.OnParametersSet();
+		if (!string.IsNullOrWhiteSpace(QuerystringAccounts))
+			Accounts = QuerystringAccounts.Split(',');
+		Model ??= new();
+	}
+	private void Save()
+	{
+		bool isValid = EditForm.EditContext!.Validate();
+		if (!isValid)
+			return;
 
-        var queryString = new QueryString();
-        queryString = queryString.Add("account", Model.Account);
-        queryString = queryString.Add(MetamaskHandler.StateKey, State ?? "");
-        NavigationManager.NavigateTo(Options.Value.LocalSignPayloadPath + queryString);
-    }
+		var queryString = new QueryString();
+		queryString = queryString.Add("account", Model.Account);
+		queryString = queryString.Add(MetamaskHandler.StateKey, State ?? "");
+		NavigationManager.NavigateTo(
+			Options.Value.LocalSignPayloadPath + queryString,
+			forceLoad: true);
+
+	}
 }
